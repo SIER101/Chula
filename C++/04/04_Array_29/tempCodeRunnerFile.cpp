16470686f524336e376880cbd902ed9590cb5380
SIER101 <73215103+SIER101@users.noreply.github.com>
2023-09-06 08:38:05 +0700

    before mid

---
+++ C++/04/04_Array_29/tempCodeRunnerFile.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m = 0;
+    cin >> n;
+    int a[n][4];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i][0] >> a[i][1] >> a[i][2] >> a[i][3];
+        for (int j = 0; j < i; j++)
+            if ((a[i][0] < a[j][2] && a[i][1] < a[j][3]) && (a[i][2] > a[j][0] && a[i][3] > a[j][1]))
+                m = max(m, ((min(a[i][2], a[j][2]) - max(a[i][0], a[j][0])) * (min(a[i][3], a[j][3]) - max(a[i][1], a[j][1]))));
+    }
+    if (m == 0)
+    {
+        cout << "No overlaps";
+        return 0;
+    }
+    cout << "Max overlapping area = " << m << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            if ((a[i][0] < a[j][2] && a[i][1] < a[j][3]) && (a[i][2] > a[j][0] && a[i][3] > a[j][1]))
+                if (max(m, ((min(a[i][2], a[j][2]) - max(a[i][0], a[j][0])) * (min(a[i][3], a[j][3]) - max(a[i][1], a[j][1])))) == m)
+                    cout << "rectangles " << i << " and " << j << endl;
+}
